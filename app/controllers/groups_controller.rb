851cdf743e85fe5f1d0a952dac160c499d24ee80
belalgamal46 <belalgamal00@gmail.com>
2023-07-08 04:22:28 +0300

    Add controller functions for creating new category records

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,4 +4,25 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all.where(user_id: current_user.id)
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user = current_user
+
+    if @group.save
+      redirect_to groups_path, notice: 'Group was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
