344d57efcf77112257ac8b325a29754ec5bfa29d
belalgamal46 <belalgamal00@gmail.com>
2023-07-08 04:01:37 +0300

    Add validations and associations to models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,17 @@
 class Group < ApplicationRecord
   belongs_to :user
+  has_many :group_trades, dependent: :destroy
+  has_many :trades, through: :group_trades
+
+  with_options presence: true do
+    validates_length_of :name, in: 3..250
+    validates :icon
+  end
+  validates_format_of :icon,
+                      with: %r{^(http(s)://.)[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)$},
+                      multiline: true
+
+  def group_trades_total
+    trades.sum(:amount)
+  end
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,3 +1,11 @@
 class Trade < ApplicationRecord
   belongs_to :user
+
+  has_many :group_trades, dependent: :destroy
+  has_many :groups, through: :group_trades
+
+  with_options presence: true do
+    validates_length_of :name, in: 1..100
+    validates_numericality_of :amount, greater_than_or_equal_to: 0
+  end
 end
